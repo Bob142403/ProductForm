5ac3d3cb3ff343f3c57b0cda9af66fcf30f80f3e
Bob2006 <sbobohoni4@gmail.com>
2023-12-14 00:32:20 +0500

    aasdas

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { SignIn } from "./components/sign-in/sign-in.component.tsx";
 import { SignUp } from "./components/sign-up/sign-up.component.tsx";
-import { BreakfastGroup } from "./components/breakfast-group/breakfast-group.component.tsx";
 import { LunchGroup } from "./components/lunch-group/lunch-group.component.tsx";
 import { DinnerGroup } from "./components/dinner-group/dinner-group.component.tsx";
 import { AuthCheck } from "./components/auth-check/auth-check.component.tsx";
